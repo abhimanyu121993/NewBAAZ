6087b02e7848de3473a3905fd8e4ae65d63cb309
KillerHeman <sharmahcool5@gmail.com>
2022-09-01 17:58:59 +0530

    api changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -172,7 +172,7 @@ class HomeController extends Controller
         try
         {
             $services = DB::table('categories as c')
-                ->select('msm.service_id as service_id', 'msm.price as service_price', 's.name as service_name', 's.image as service_image')
+                ->select('msm.service_id as service_id', 'msm.price as service_price', 's.name as service_name', 's.image as service_image', 's.desc as service_desc')
                 ->join('services as s', 's.cid', 'c.id')
                 ->join('model_service_maps as msm', 'msm.service_id', 's.id')
                 ->join('brand_models as bm', 'bm.id', 'msm.model_id')
