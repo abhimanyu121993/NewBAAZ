72e71f1c01285ba296f8e0c7c7722edf3fc0662b
KillerHeman <sharmahcool5@gmail.com>
2022-09-13 11:48:31 +0530

    first

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,103 +15,97 @@ class CartController extends Controller
 {
     public function showCart(Request $req)
     {
-        try{ 
-        $cartItems = Cart::where('user_id',$req->id)->get();
-        if($cartItems){
-            $result = [
-                'data' => $cartItems,
-                'message' => 'Items showing In cart',
-                'status' => 200,
-                'error' => NULL
-            ];
+        $req->validate([
+            'user_id' => 'required'
+        ]);
+        try {
+            $cartItems = Cart::where('user_id', $req->user_id)->get();
+            if ($cartItems) {
+                $result = [
+                    'data' => $cartItems,
+                    'message' => 'Cart detail found',
+                    'status' => 200,
+                    'error' => NULL
+                ];
+            } else {
+                $result = [
+                    'data' => NULL,
+                    'message' => 'Empty Cart',
+                    'status' => 200,
+                    'error' => [
+                        'message' => 'Server Error',
+                        'code' => 305,
+                    ]
+                ];
+            }
+            return response()->json($result);
+        } catch (Exception $ex) {
+            $url = URL::current();
+            Error::create(['url' => $url, 'message' => $ex->getMessage()]);
         }
-        else{
-            $result = [
-                'data' => NULL,
-                'message' => 'Items showing In cart',
-                'status' => 200,
-                'error' => [
-                    'message' => 'Server Error',
-                    'code' => 305,
-                ]
-            ];
-        }
-        return response()->json($result);
-    }
-    catch (Exception $ex)
-    {
-        $url=URL::current();
-        Error::create(['url'=>$url,'message'=>$ex->getMessage()]);
-    } 
     }
 
-public function addToCart(Request $request)
+    public function addToCart(Request $request)
     {
-            try
-            {
-                $addTocart = Cart::create(['user_id' => $request->user_id, 'model_id'=>$request->model_id ,'service_id' => $request->service_id, 'quantity' =>1]);
-                if ($addTocart)
-                {
-                    $result = [
-                        'data' => $addTocart,
-                        'message' => 'Service added to cart',
-                        'status' => 200,
-                        'error' => NULL
-                    ];
-                }
-                else
-                {
-                    $result = [
-                        'data' => NULL,
-                        'message' => 'Service not added to cart',
-                        'status' => 200,
-                        'error' => [
-                            'message' => 'Server Error',
-                            'code' => 305,
-                        ]
-                    ];
-                }
-                return response()->json($result);
+        $request->validate([
+            'user_id' => 'required',
+            'model_id' => 'required',
+            'service_id' => 'required'
+        ]);
+        try {
+            $addTocart = Cart::create(['user_id' => $request->user_id, 'model_id' => $request->model_id, 'service_id' => $request->service_id, 'quantity' => 1]);
+            if ($addTocart) {
+                $result = [
+                    'data' => $addTocart,
+                    'message' => 'Service added to cart',
+                    'status' => 200,
+                    'error' => NULL
+                ];
+            } else {
+                $result = [
+                    'data' => NULL,
+                    'message' => 'Service not added to cart',
+                    'status' => 200,
+                    'error' => [
+                        'message' => 'Server Error',
+                        'code' => 305,
+                    ]
+                ];
             }
-            catch (Exception $ex)
-            {
-                $url=URL::current();
-                Error::create(['url'=>$url,'message'=>$ex->getMessage()]);
-            }   
-        }   
-public function deleteCart(Request $req)
-{
-    try {
-        $res = Cart::find($req->id)->delete();
-        if ($res)
-        {
-            $result = [
-                'data' => $res,
-                'message' => 'Cart deleted sucessfully',
-                'status' => 200,
-                'error' => NULL
-            ];
+            return response()->json($result);
+        } catch (Exception $ex) {
+            $url = URL::current();
+            Error::create(['url' => $url, 'message' => $ex->getMessage()]);
         }
-        else
-        {
-            $result = [
-                'data' => NULL,
-                'message' => 'Cart not deleted sucessfully',
-                'status' => 200,
-                'error' => [
-                    'message' => 'Server Error',
-                    'code' => 305,
-                ]
-            ];
-        }
-        return response()->json($result);
     }
-    catch(Exception $ex)
+    public function deleteCart(Request $req)
     {
-        $url=URL::current();
-        Error::create(['url'=>$url,'message'=>$ex->getMessage()]);
-    }  
-} 
+        try {
+            $res = Cart::find($req->user_id)->delete();
+            if ($res) {
+                $result = [
+                    'data' => $res,
+                    'message' => 'Cart deleted sucessfully',
+                    'status' => 200,
+                    'error' => NULL
+                ];
+            } else {
+                $result = [
+                    'data' => NULL,
+                    'message' => 'Cart not deleted',
+                    'status' => 200,
+                    'error' => [
+                        'message' => 'Server Error',
+                        'code' => 305,
+                    ]
+                ];
+            }
+            return response()->json($result);
+        } catch (Exception $ex) {
+            $url = URL::current();
+            Error::create(['url' => $url, 'message' => $ex->getMessage()]);
+        }
+    }
 
     // decrement brand and delete Quantity one by one
 
