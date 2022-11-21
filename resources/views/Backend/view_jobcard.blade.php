b71d67ffd126b3121cf6aadf29933666fd320832
KillerHeman <sharmahcool5@gmail.com>
2022-11-21 15:44:20 +0530

    download option

--- resources/views/Backend/view_jobcard.blade.php
+++ resources/views/Backend/view_jobcard.blade.php
@@ -70,7 +70,7 @@
     @if ($jobcard)
         @if ($jobcard->status == 1)
         <center><button style="border:none;" onclick="window.print()">Print</button>
-            <button class="btn btn-primary" id="download">Download</button>
+            <button class="btn btn-primary" id="download" style="background-color: #f1c26c;color:white;">Download</button>
         </center><br>
             <div class="container" id="jobcardcontent">
                 <!-- <p class="text-center mt-2">Invoice</p> -->
