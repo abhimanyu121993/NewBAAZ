8b21e6e27a3fb27ffe2661beb1372f08ed3f0a31
KillerHeman <sharmahcool5@gmail.com>
2022-12-26 16:49:34 +0530

    api bug fixes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,6 @@ class AuthController extends Controller
 {
     public function userLogin(Request $req)
     {
-        Log::info($req->all());
         $req->validate([
             'mobileno' => 'required|min:10|max:10',
             'authentication' => 'required'
