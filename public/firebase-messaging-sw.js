e60264c2938194417b863d3c018abfa1ee4b4c1e
komal <komalsharm10786@gmail.com>
2023-01-20 15:45:03 +0530

    2

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,11 +7,11 @@ firebase.initializeApp({
     // projectId: "baazapp-9d05b",
     // messagingSenderId: "317918604459",
     // appId: "1:317918604459:android:7a768864d755f25bf7dbee",
-    apiKey: "AIzaSyDE6hJnUlCwrsItIwGzgANXEadx6ITNNlo",
-    authDomain: "baazapp-9d05b.firebaseapp.com",
-    projectId: "baazapp-9d05b",
-    messagingSenderId: "317918604459",
-    appId: "1:317918604459:android:48d05054d4661d6ff7dbee",
+    // apiKey: "AIzaSyDE6hJnUlCwrsItIwGzgANXEadx6ITNNlo",
+    // authDomain: "baazapp-9d05b.firebaseapp.com",
+    // projectId: "baazapp-9d05b",
+    // messagingSenderId: "317918604459",
+    // appId: "1:317918604459:android:48d05054d4661d6ff7dbee",
 });
 
 const messaging = firebase.messaging();
