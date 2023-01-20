b803968319f5a770fbfcbab247a352b29b6d3033
komal <komalsharm10786@gmail.com>
2023-01-20 18:32:45 +0530

    komal nofi

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,7 +6,7 @@ firebase.initializeApp({
     authDomain: "baazapp-9d05b.firebaseapp.com",
     projectId: "baazapp-9d05b",
     messagingSenderId: "317918604459",
-    appId: "1:317918604459:android:7a768864d755f25bf7dbee",
+    appId: "1:317918604459:android:48d05054d4661d6ff7dbee",
 });
 
 const messaging = firebase.messaging();
