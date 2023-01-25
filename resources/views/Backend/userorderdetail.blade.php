a40de3f94da79674a99a1ca1228f5fd7b0bb1c66
KillerHeman <sharmahcool5@gmail.com>
2023-01-25 16:51:20 +0530

    first

--- resources/views/Backend/userorderdetail.blade.php
+++ resources/views/Backend/userorderdetail.blade.php
@@ -34,7 +34,7 @@
                                 <td><img src="{{ asset($orderd->servicetype->image ?? '') }}" class="me-75 bg-light-danger"
                                         style="height:35px;width:35px;" /></td>
                                 <td>{{ $orderd->servicetype->name ?? '' }}</td>
-                                <td>{{ $orderd->modelmapservice->where('service_id', $orderd->servicetype->id)->first()->discounted_price ?? '' }}
+                                <td>{{ $orderd->modelmapservice->discounted_price ?? '' }}
                                 </td>
                             </tr>
                         @endforeach
@@ -47,13 +47,13 @@
                                     @endif
                                 @endif
 
-                                <td><a href="{{ route('Backend.jobcard.edit', $order->id) }}" class="btn btn-primary">Job
+                                <td colspan="2" ><a href="{{ route('Backend.jobcard.edit', $order->id) }}" class="btn btn-primary">Job
                                         Card</a></td>
                             </tr>
                         @endrole
                     @else
                         <tr>
-                            <td colspan="100%" class="text-center">Service not available for this order</td>
+                            <td colspan="3" class="text-center">Service not available for this order</td>
                         </tr>
                     @endif
                 </tbody>
@@ -71,6 +71,7 @@
     <script src="{{ asset('Backend/assets/vendors/js/pickers/flatpickr/flatpickr.min.js') }}"></script> --}}
     <script>
         $(document).ready(function() {
+            $.fn.dataTable.ext.errMode = 'none';
             $('#orderuserdetail').DataTable({
                 dom: 'Bfrtip',
                 buttons: [
